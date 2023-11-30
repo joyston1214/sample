19e21d2e75a32a5c44e136df6a3febee063135fd
joyston1214 <fjoyston@gmail.com>
2023-11-30 13:01:48 +0530

    1c

---
+++ zzz/src/test/java/Ex/Ts1Test.java
@@ -0,0 +1,12 @@
+package Ex;
+
+import org.testng.annotations.Test;
+
+public class Ts1Test {
+	
+	@Test
+	public void run1Test() {
+		System.out.println("---Ts1---");
+	}
+
+}
---
+++ zzz/src/test/java/Ex/Ts2Test.java
@@ -0,0 +1,12 @@
+package Ex;
+
+import org.testng.annotations.Test;
+
+public class Ts2Test {
+	
+	@Test
+	public void run2Test() {
+		System.out.println("---Ts2---");
+	}
+
+}
